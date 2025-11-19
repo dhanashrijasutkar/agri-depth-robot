870c2ca4fef060f1520ea54e1283a36e404658cf
Dhanashri Jasutkar <dhanashrijasutkar16@gmail.com>
2025-11-19 22:49:36 +0530

    feat: add config definitions

---
+++ arduino/config.h
@@ -0,0 +1,21 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define TRIG_PIN D6
+#define ECHO_PIN D7
+#define SERVO_PIN D5
+
+#define IN1 D1
+#define IN2 D2
+#define IN3 D3
+#define IN4 D4
+
+#define LED_PIN D8
+#define WIFI_LED_PIN D0
+#define SOIL_PIN A0
+
+#define PROBE_POS_SHALLOW 30
+#define PROBE_POS_MID     60
+#define PROBE_POS_DEEP    90
+
+#endif
